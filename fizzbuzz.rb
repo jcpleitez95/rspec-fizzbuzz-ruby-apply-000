762c7587f5316a42c9998b87ef7b308b30746fe7
essential-blog-3393 <jcpleitez@outlook.com>
2020-09-21 16:09:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,8 @@
 def fizzbuzz (number)
-  3 == "Fizz"
-  5 == "Buzz"
-  15 == "FizzBuzz"
+  if number % 3 == 0 
+    "Fizz"
+  if number % 5 == 0 
+    "Buzz"
+  if number % [3,5] == 0 
+    "FizzBuzz"
 end
